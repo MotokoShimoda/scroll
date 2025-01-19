8cdee9921e42dd720e5c41ce0763dcbe084d671c
motokoshimoda <katzemochi1523@gmail>
2025-01-19 16:09:55 +0900

    revision2

--- scroll.js
+++ scroll.js
@@ -1,24 +1,23 @@
-const boxes = document.querySelectorAll(".scroll-in");
+document.addEventListener("DOMContentLoaded", () => {
+  const boxes = document.querySelectorAll(".scroll-in");
 
-const callback = (entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("displayed");
-
-      setTimeout(() => {
-        entry.target.classList.add("hide");
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("displayed");
+      } else {
         entry.target.classList.remove("displayed");
-      }, 1500);
-    }
-  });
-};
+      }
+    });
+  };
 
-const options = {
-  root: null,
-  rootMargin: "0px 0px -10% 0px",
-  threshold: 0.1,
-};
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
 
-const observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(callback, options);
 
-boxes.forEach((box) => observer.observe(box));
+  boxes.forEach((box) => observer.observe(box));
+});
